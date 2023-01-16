085d99c35baae03a77a18c53c35653bf36aec188
Shagun Srivastava <shagun@Shaguns-MacBook-Pro-2.local>
2023-01-16 01:58:17 -0800

    Add existing project files to Git

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import openai
+
+openai.api_key = "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"
+model = "text-davinci-003"
+
+st.write("# Explain Like I'm 5")
+input_str = st.text_input("What do you want to know?", key="input")
+
+st.write("""
+<script>
+    document.getElementById("input").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("submit").click();
+    }
+    });
+</script>
+""", unsafe_allow_html=True)
+
+if not input_str.endswith("?"):
+    input_str += "?"
+
+if st.button("Submit"):
+    input_str = "Could you explain this to a five year old " + input_str
+    response = openai.Completion.create(engine=model, prompt=input_str, max_tokens=1024, temperature=0.5)
+    response_str = response['choices'][0]['text']
+    st.success(response_str)
